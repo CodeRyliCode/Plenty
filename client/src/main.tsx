798d9fa08cb3746b8d6f26231fc0d0e9eac5ade2
coderylicode <rylidavis94@gmail.com>
2024-07-29 19:10:29 -0700

    add form boilerplate

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,6 +12,5 @@ if(!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} children={<App/>}></ClerkProvider>
-    <App />
   </React.StrictMode>,
 )

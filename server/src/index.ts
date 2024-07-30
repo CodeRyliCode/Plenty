02dd3732b4c14882fcea8fd30993194dcd2316d8
coderylicode <rylidavis94@gmail.com>
2024-07-30 14:02:21 -0700

    add api endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,13 +29,16 @@
 
 import express, {Express} from "express"
 import mongoose from "mongoose";
-
+import financialRecordRouter from "./routes/financial-records"
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 const mongoURI: string =  "mongodb+srv://rylidavis94:v0mMcXRKsRIELn6Y@plenty.ggjduyv.mongodb.net/?retryWrites=true&w=majority&appName=Plenty";
 mongoose.connect(mongoURI).then(() => console.log("CONNECTED TO MONGODB")).catch((err) => console.error("Failed to Connect to Mongodb:", err))
+
+app.use("/financial-records", financialRecordRouter);
+
 app.listen(port, () => {
     console.log(`Server Running on Port ${port}`)
 })

daf753e2c1861e8ca7b0a08c39db4eb9f8ff43ab
coderylicode <rylidavis94@gmail.com>
2024-07-30 17:31:55 -0700

    add table and editable cell

--- client/src/context/financial-record-context.tsx
+++ client/src/context/financial-record-context.tsx
@@ -1,6 +1,7 @@
-import React, {createContext, useContext, useState} from "react"
+import React, {createContext, useContext, useEffect, useState} from "react"
+import {useUser} from "@clerk/clerk-react";
 
-interface FinancialRecord {
+export interface FinancialRecord {
     id?: string;
     userId: string;
     date: Date;
@@ -21,7 +22,24 @@ export const FinancialRecordsContext = createContext<FinancialRecordsContextType
 
 export const FinancialRecordsProvider = ({children,}: {children: React.ReactNode}) => {
     const [records, setRecords] = useState<FinancialRecord[]>([]);
-const addRecord = async (record: FinancialRecord) => {
+const {user} = useUser();
+const fetchRecords = async () => {
+    if(!user) return;
+    const response = await fetch(
+        `http://localhost:3001/financial-records/getAllByUserID/${user?.id}`
+    );
+    if(response.ok) {
+        const records = await response.json()
+        console.log('records',records)
+        setRecords(records);
+    }
+};
+
+useEffect(()=> {
+    fetchRecords();
+},[user])
+
+    const addRecord = async (record: FinancialRecord) => {
     const response = await fetch("http://localhost:3001/financial-records", {
         method: "POST",
         body: JSON.stringify(record),
